8f5307ac0f692c575334783675baaf4e1df32c25
Camilo Velasquez <camilo.velasquezp@gmail.com>
2024-07-19 23:32:05 -0500

    perf: add environment variable

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    LOCAL_API_URL: "http://localhost:3000/api/sales",
+  },
+};
 
 export default nextConfig;
