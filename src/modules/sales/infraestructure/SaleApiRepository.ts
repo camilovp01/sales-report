26a046dabf7ba1d10c4b92dea6a16d02c8487b37
camilo <camilo@camilo>
2024-07-21 15:18:34 -0500

    refactor: fix build

--- src/modules/sales/infraestructure/SaleApiRepository.ts
+++ src/modules/sales/infraestructure/SaleApiRepository.ts
@@ -5,7 +5,7 @@ import { SaleRepository } from "../domain/Sale.repository";
 
 export class SaleApiRepository implements SaleRepository {
   async getAll(): Promise<Sale[]> {
-    const response = await fetch(localApiUrl, {
+    const response = await fetch(`${localApiUrl}/api/sales`, {
       method: "GET",
     });
     const data = await response.json();
