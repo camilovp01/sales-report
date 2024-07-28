6edc5e3a061701fa0b3494a3a9c35f7a721af6f9
Camilo Velasquez <camilo.velasquezp@gmail.com>
2024-07-28 17:14:50 -0500

    feat: add bread crumbs

--- src/components/filter/checkboxFilter/CheckboxFilter.tsx
+++ src/components/filter/checkboxFilter/CheckboxFilter.tsx
@@ -24,22 +24,23 @@ export default function CheckboxFilter({
   const values = Object.values(options);
   const keys = Object.keys(options);
   const initialCheckedState = keys.map(
-    (key) => defaultChecked[key as keyof typeof defaultChecked] || false,
+    (key) => defaultChecked[key as keyof typeof defaultChecked] || false
   );
 
   const [showFilter, setShowFilter] = useState<boolean>(false);
   const { changeCheckbox } = useAppContext();
   const [checkedState, setCheckedState] = useState(initialCheckedState);
   const [filterIsValid, setFilterIsValid] = useState<boolean>(false);
+  const [breadcrumbs, setbreadcrumbs] = useState<boolean[]>();
 
   const handleOnChange = (position: number) => {
     const updatedCheckedState = checkedState.map((item, index) =>
-      index === position ? !item : item,
+      index === position ? !item : item
     );
     if (!updatedCheckedState.includes(true)) {
       const result = createPartialFiltersObject(
         keys,
-        updatedCheckedState,
+        updatedCheckedState
       ) as Filters;
 
       changeCheckbox({ ...result });
@@ -55,61 +56,78 @@ export default function CheckboxFilter({
 
   const createPartialFiltersObject = (
     keys: string[],
-    checkedState: boolean[],
+    checkedState: boolean[]
   ): Partial<Filters> => {
     return Object.fromEntries(
-      keys.map((key, index) => [key, checkedState[index]]),
+      keys.map((key, index) => [key, checkedState[index]])
     ) as Partial<Filters>;
   };
 
   useEffect(() => {
     setFilterIsValid(Object.values(checkedState).includes(true));
+    setbreadcrumbs(checkedState);
   }, [checkedState]);
 
   return (
-    <div className={styles["container__wrapper"]}>
-      <button
-        onClick={() => setShowFilter(true)}
-        className={styles["container__wrapper__button"]}
-      >
-        Filtrar
-      </button>
-      <SlidersHorizontal
-        onClick={() => setShowFilter(true)}
-        size={16}
-        className={styles["container__wrapper__icon"]}
-      />
-      <div
-        className={`${showFilter ? styles["container__wrapper__options--show"] : styles["container__wrapper__options--hide"]}`}
-      >
-        <div className={styles["container__wrapper__close"]}>
-          <div className={styles["container__wrapper__title"]}>
-            <p>Filtrar</p>
-          </div>
-          <X
+    <div className={styles["filter-container"]}>
+      <div className={styles["container"]}>
+        <div className={styles["container-wrapper"]}>
+          <button
+            onClick={() => setShowFilter(true)}
+            className={styles["container-wrapper__button"]}
+          >
+            Filtrar
+          </button>
+          <SlidersHorizontal
+            onClick={() => setShowFilter(true)}
             size={16}
-            className={styles["container__wrapper__icon"]}
-            onClick={() => setShowFilter(false)}
+            className={styles["container-wrapper__icon"]}
           />
-        </div>
-        {values.map((value, i) => (
-          <div key={value} className={styles["container__wrapper__option"]}>
-            <input
-              type="checkbox"
-              id={value}
-              checked={checkedState[i]}
-              onChange={() => handleOnChange(i)}
-            />
-            <label htmlFor={value}>{value}</label>
+          <div
+            className={`${showFilter ? styles["container-wrapper__options--show"] : styles["container-wrapper__options--hide"]}`}
+          >
+            <div className={styles["container-wrapper__close"]}>
+              <div className={styles["container-wrapper__title"]}>
+                <p>Filtrar</p>
+              </div>
+              <X
+                size={16}
+                className={styles["container-wrapper__icon"]}
+                onClick={() => setShowFilter(false)}
+              />
+            </div>
+            {values.map((value, i) => (
+              <div key={value} className={styles["container-wrapper__option"]}>
+                <input
+                  type="checkbox"
+                  id={value}
+                  checked={checkedState[i]}
+                  onChange={() => handleOnChange(i)}
+                />
+                <label htmlFor={value}>{value}</label>
+              </div>
+            ))}
+            <button
+              disabled={!filterIsValid}
+              className={`${styles["container__button"]}`}
+              onClick={submit}
+            >
+              Aplicar
+            </button>
           </div>
-        ))}
-        <button
-          disabled={!filterIsValid}
-          className={`${styles["container__button"]}`}
-          onClick={submit}
-        >
-          Aplicar
-        </button>
+        </div>
+      </div>
+      <div className={styles["filter"]}>
+        {breadcrumbs &&
+          breadcrumbs.map((value, i) => {
+            if (value) {
+              return (
+                <span className={styles["filter__item"]} key={i}>
+                  {values[i]}
+                </span>
+              );
+            }
+          })}
       </div>
     </div>
   );
