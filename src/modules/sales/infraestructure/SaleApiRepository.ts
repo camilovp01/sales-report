0f4f422120d23ea8a30b80d6fda4d79be546d12c
Camilo Velasquez <camilo.velasquezp@gmail.com>
2024-08-02 08:33:38 -0500

    fix: change enpoint

--- src/modules/sales/infraestructure/SaleApiRepository.ts
+++ src/modules/sales/infraestructure/SaleApiRepository.ts
@@ -1,11 +1,11 @@
-import { LOCAL_API_URL } from "@/constants/constants";
+import { API_URL, LOCAL_API_URL } from "@/constants/constants";
 
 import { Sale } from "../domain/Sale";
 import { SaleRepository } from "../domain/Sale.repository";
 
 export class SaleApiRepository implements SaleRepository {
   async getAll(): Promise<Sale[]> {
-    const response = await fetch(`${LOCAL_API_URL}/api/sales`, {
+    const response = await fetch(`${API_URL}`, {
       method: "GET",
     });
     const data = await response.json();
